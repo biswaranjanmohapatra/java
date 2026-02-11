480655218a67b93688dcf7c74791d0a25970dd01
Biswa <biswaranjanmohapatra371@gmail.com>
2026-02-11 17:39:09 +0530

    intial commit

---
+++ src/_01_array/reversearr.java
@@ -0,0 +1,23 @@
+package _01_array;
+
+import java.util.Arrays;
+
+public class reversearr {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        rev(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void rev(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
+
